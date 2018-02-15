b8c11b73f6c26a1344bf89ea0f8911604e6293db
camilabelen <ccornejoss@gmail.com>
2018-02-14 23:26:16 -0400

    accediendo a api - busqueda de pokemones segun su tipo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-const pokemonContainer = document.getElementById('pokemonContainer');
+$('form').on('submit', function(e) {
+  e.preventDefault();
+
+  var search = $('#search').val().replace(/\s/g, '');
+  search = search.split(',');
+  console.log(search);
+
+  var pokeTypes = search.map(function(names) {
+    return $.ajax({
+      url: 'http://pokeapi.salestock.net/api/v2/type/' + search,
+      datatype: 'json',
+      method: 'GET'
+    });
+  });
+
+  $.when.apply(null, pokeTypes)
+    .then(function() {
+      var pokeTypes = Array.prototype.slice.call(arguments);
+      console.log(pokeTypes);
+    });
+});
+
+/*
+function getDoubleDmgTypes(pokeTypes) {
+  pokeTypes = pokeTypes.map(function(types) {
+    return types[0].damage_relations.double_damage_from;
+  });
+  console.log(pokeTypes);
+}
+*/
+
